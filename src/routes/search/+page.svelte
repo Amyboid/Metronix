1bb0070dfcd48bb3845889e850df614ba46e40c6
Amyboid <134444582+Amyboid@users.noreply.github.com>
2025-11-30 22:16:29 +0530

    add open search

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
+	let param = page.url.searchParams.get('q');
+	console.log(param);
+</script>
+
+<div>
+	<input value={param} />
+</div>
